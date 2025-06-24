5531f2dc5a6a45eb7cf25539b012c845a296f1dc
jp <ssupawej@gmail.com>
2025-06-24 16:14:26 +0700

    finish listening queries

--- main.go
+++ main.go
@@ -1,17 +1,43 @@
-package dnsproxy
+package main
 
-/*
 import (
 	"fmt"
 	"log"
 	"net"
+
 	"github.com/miekg/dns"
-
 )
-*/
-
-const port int = 53
 
 func main() {
-	//addr, err := net.ListenUDP()
+	addr := net.UDPAddr{
+		Port: 53,
+		IP:   net.ParseIP("0.0.0.0"),
+	}
+
+	conn, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		log.Fatalf("Failed to listen on port 53: %v", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("DNS Proxy listening on port 53...")
+
+	buffer := make([]byte, 1024)
+
+	for {
+		n, clientAddr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			log.Printf("Error reading UDP: %v", err)
+			continue
+		}
+
+		var msg dns.Msg
+		msg.Unpack(buffer[:n])
+
+		fmt.Printf("From %s:\n", clientAddr)
+		for _, q := range msg.Question {
+			fmt.Printf(" - Question: %s, Type: %s\n", q.Name, dns.TypeToString[q.Qtype])
+		}
+		println("--------------------------------------")
+	}
 }
